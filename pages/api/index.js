f132cbbef0d4a2c1edc5d39444c957fdfa2a9657
notsobad <tomomichi.onishi@gmail.com>
2020-09-12 17:06:57 +0900

    rename and enable parameter setting

--- pages/api/index.js
+++ pages/api/index.js
@@ -10,14 +10,24 @@ if(process.env.AWS_LAMBDA_FUNCTION_VERSION){
 }
 
 export default async (req, res) => {
+  if(!req.query.url) {
+    return res.status(404).end();
+  }
   const URL = req.query.url;
+
+  // 各種パラメータ（指定なければデフォルト値）
+  const slowMo = req.query.slowMo ? Number(req.query.slowMo) : 0;
+  const fullPage = (req.query.fullPage == 'true') ? true : false;
+  const maxAge = req.query.maxAge ? Number(req.query.maxAge) : 60*60*24;
+  const viewportWidth = req.query.viewportWidth ? Number(req.query.viewportWidth) : 1200;
+  const viewportHeight = req.query.viewportHeight ? Number(req.query.viewportHeight) : 800;
 
   if(process.env.AWS_LAMBDA_FUNCTION_VERSION){
     await chrome.font('https://raw.githack.com/googlei18n/noto-cjk/master/NotoSansJP-Regular.otf');
   }
 
   const browser = await puppeteer.launch({
-    slowMo: 150,
+    slowMo: slowMo,
     args: chrome.args,
     executablePath: await chrome.executablePath,
     headless: chrome.headless,
@@ -25,19 +35,19 @@ export default async (req, res) => {
   const page = await browser.newPage();
 
   await page.setViewport({
-    width: 1200,
-    height: 800,
+    width: viewportWidth,
+    height: viewportHeight,
   });
   await page.goto(URL);
 
   const imgBinary = await page.screenshot({
     encoding: 'binary',
-    // fullPage: true,
+    fullPage: fullPage,
   });
   await browser.close();
 
   res.setHeader('Content-Type', 'image/png');
   res.setHeader('Content-Length', imgBinary.length);
-  res.setHeader('Cache-Control', `public, s-maxage=${60*60}, stale-while-revalidate`);
+  res.setHeader('Cache-Control', `public, s-maxage=${maxAge}, stale-while-revalidate`);
   res.end(imgBinary, "binary");
 }
