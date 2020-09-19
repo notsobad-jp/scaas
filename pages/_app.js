d6fa10fc634c4b5dd0eeb003eb354c8d0e21612a
notsobad <tomomichi.onishi@gmail.com>
2020-09-19 17:38:36 +0900

    use scaas as og-image

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ const App = ({ Component, pageProps }) => {
         <meta property="og:title" content="Scaas - 簡単・便利なスクリーションショットAPIサービス" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://scaas.vercel.app" />
-        <meta property="og:image" content="https://scaas.vercel.app/images/ogp.png" />
+        <meta property="og:image" content="https://scaas.vercel.app/api?url=https://scaas.vercel.app&viewportWidth=1200&viewportHeight=600&maxAge=2592000" />
         <meta property="og:site_name" content="Scaas - 簡単・便利なスクリーションショットAPIサービス" />
         <meta property="og:description" content="Scaas(スキャース)は、URLを指定するだけで画面キャプチャを使えるシンプルなスクリーンショット作成サービスです" />
         <meta property="og:locale" content="ja_JP" />
