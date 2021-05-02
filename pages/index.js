0a9b8312c73608d8321767407809a769b25c7452
notsobad <tomomichi.onishi@gmail.com>
2021-05-02 14:49:36 +0900

    fix header z-index

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Index() {
   return (
     <div>
 
-      <header className="absolute w-full text-gray-700 body-font">
+      <header className="absolute w-full text-gray-700 body-font z-10">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <img src="/images/logo.png" alt="" style={{width: 40}} />
